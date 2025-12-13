c9d2c90332cdd9f783277f1df592cdf4cf3be199
grosh <dima.grosh@gmail.com>
2025-12-13 16:36:29 +0300

    update form to array

--- apps/web/src/features/alco-form/model/alcoFormModel.ts
+++ apps/web/src/features/alco-form/model/alcoFormModel.ts
@@ -1,31 +1,63 @@
-import { reatomEnum, reatomField, reatomForm, withField } from "@reatom/core";
+import { experimental_fieldArray, reatomEnum, reatomField, reatomForm, withField } from "@reatom/core";
 import { z } from "zod";
 import { ALCOHOL_BOTTLE_TYPE_MAP, ALCOHOL_PERCENTAGE_MAP, ALCOHOLS, BOTTLE_SIZE_ML_MAP, TYPE_OF_BOTTLE, type SizeOfBottle } from "../lib";
 
-export const alcoFormAtom = reatomForm(
+// export const alcoFormAtom = reatomForm(
+//     {
+//         name: reatomEnum(ALCOHOLS, { initState: 'Beer' }).extend(withField()),
+//         percentage: reatomField(ALCOHOL_PERCENTAGE_MAP['Beer']),
+//         typeOfBottle: reatomEnum(TYPE_OF_BOTTLE, { initState: ALCOHOL_BOTTLE_TYPE_MAP['Beer'][0] }).extend(withField()),
+//         sizeOfBottle: reatomField(BOTTLE_SIZE_ML_MAP['Bottle'][0] as SizeOfBottle),
+//         count: 1,
+//     },
+//     {
+//         validateOnBlur: true,
+//         schema: z.object({
+//             name: z.literal(ALCOHOLS),
+//             percentage: z.number(),
+//             typeOfBottle: z.literal(TYPE_OF_BOTTLE),
+//             count: z.number().min(0),
+//         })
+//     }
+// )
+
+// alcoFormAtom.fields.name.subscribe((value) => {
+//     alcoFormAtom.fields.percentage.set(ALCOHOL_PERCENTAGE_MAP[value]);
+//     alcoFormAtom.fields.typeOfBottle.set(ALCOHOL_BOTTLE_TYPE_MAP[value][0]);
+// });
+
+// alcoFormAtom.fields.typeOfBottle.subscribe((value) => {
+//     alcoFormAtom.fields.sizeOfBottle.set(BOTTLE_SIZE_ML_MAP[value][0] as SizeOfBottle);
+// });
+
+export const alcoFormListAtom = reatomForm(
     {
-        name: reatomEnum(ALCOHOLS, { initState: 'Beer' }).extend(withField()),
-        percentage: reatomField(ALCOHOL_PERCENTAGE_MAP['Beer']),
-        typeOfBottle: reatomEnum(TYPE_OF_BOTTLE, { initState: ALCOHOL_BOTTLE_TYPE_MAP['Beer'][0] }).extend(withField()),
-        sizeOfBottle: reatomField(BOTTLE_SIZE_ML_MAP['Bottle'][0] as SizeOfBottle),
-        count: 1,
+        drinks: experimental_fieldArray({
+            initState: [{
+                name: "Beer",
+                percentage: ALCOHOL_PERCENTAGE_MAP['Beer'],
+                typeOfBottle: ALCOHOL_BOTTLE_TYPE_MAP['Beer'][0],
+                sizeOfBottle: BOTTLE_SIZE_ML_MAP['Bottle'][0],
+                count: 1,
+            }],
+            create: ({ name, percentage, typeOfBottle, sizeOfBottle, count }) => ({
+                name,
+                percentage,
+                typeOfBottle,
+                sizeOfBottle,
+                count,
+            })
+        })
     },
     {
         validateOnBlur: true,
-        schema: z.object({
+        schema: z.array(
+            z.object({
             name: z.literal(ALCOHOLS),
             percentage: z.number(),
             typeOfBottle: z.literal(TYPE_OF_BOTTLE),
             count: z.number().min(0),
         })
+        )
     }
 )
-
-alcoFormAtom.fields.name.subscribe((value) => {
-    alcoFormAtom.fields.percentage.set(ALCOHOL_PERCENTAGE_MAP[value]);
-    alcoFormAtom.fields.typeOfBottle.set(ALCOHOL_BOTTLE_TYPE_MAP[value][0]);
-});
-
-alcoFormAtom.fields.typeOfBottle.subscribe((value) => {
-    alcoFormAtom.fields.sizeOfBottle.set(BOTTLE_SIZE_ML_MAP[value][0] as SizeOfBottle);
-});
--- apps/web/src/features/alco-form/ui/AlcoForm.tsx
+++ apps/web/src/features/alco-form/ui/AlcoForm.tsx
@@ -1,11 +1,12 @@
 import { reatomComponent } from "@reatom/react";
-import { alcoFormAtom } from "../model";
+import { alcoFormListAtom } from "../model";
 import type { FormEvent } from "react";
 import { Button, Flex, Input, Select, Typography } from "antd";
-import { ALCO_OPTIONS, ALCOHOL_BOTTLE_TYPE_OPTIONS_MAP, BOTTLE_SIZE_OPTIONS_MAP } from "../lib";
+import { ALCO_OPTIONS, ALCOHOL_BOTTLE_TYPE_MAP, ALCOHOL_BOTTLE_TYPE_OPTIONS_MAP, ALCOHOL_PERCENTAGE_MAP, ALCOHOLS, BOTTLE_SIZE_ML_MAP, BOTTLE_SIZE_OPTIONS_MAP, type AlcoholType } from "../lib";
 
 export const AlcoForm = reatomComponent(() => {
-    const { fields, submit } = alcoFormAtom;
+    // const { fields, submit } = alcoFormAtom;
+    const { fields: drinksFields, submit } = alcoFormListAtom
 
     const onSubmit = (e: FormEvent<HTMLFormElement>) => {
         e.preventDefault();
@@ -13,49 +14,57 @@ export const AlcoForm = reatomComponent(() => {
         submit();
     }
 
-    // const onAddDrink = () => {
-    //     fields.drinks.create({name: 'Beer', percentage: ALCOHOL_PERCENTAGE_MAP['Beer'], grams: 100});
-    // }
+    const onAddDrink = () => {
+        drinksFields.drinks.create({ 
+            name: "Beer",
+            percentage: ALCOHOL_PERCENTAGE_MAP['Beer'],
+            typeOfBottle: ALCOHOL_BOTTLE_TYPE_MAP['Beer'][0],
+            sizeOfBottle: BOTTLE_SIZE_ML_MAP['Bottle'][0],
+            count: 1 
+        });
+    }
 
     return (
         <form onSubmit={onSubmit}>
             <Flex vertical gap={16}>
-                  <Flex wrap="wrap" gap={8}>
+                  {drinksFields.drinks.array().map((drink) => (
+                    <Flex wrap="wrap" gap={8}>
                         <Flex style={{ minWidth: 200 }} vertical gap={4} flex={1}>
                             <Typography.Title level={5}>Alco Name</Typography.Title>
                             <Select 
-                                value={fields.name.value()} 
-                                onChange={(value) => fields.name.set(value)}
+                                value={drink.name.value()} 
+                                onChange={(value) => drink.name.set(value)}
                                 options={ALCO_OPTIONS}
                             />
                         </Flex>
                         <Flex style={{ minWidth: 200 }} vertical gap={4} flex={1}>
                             <Typography.Title level={5}>Alco Percentage</Typography.Title>
-                            <Input type="number" value={fields.percentage.value()} onChange={(e) => fields.percentage.set(Number(e.target.value))} />
+                            <Input type="number" value={drink.percentage.value()} onChange={(e) => drink.percentage.set(Number(e.target.value))} />
                         </Flex>
                         <Flex style={{ minWidth: 200 }} vertical gap={4} flex={1}>
                             <Typography.Title level={5}>Type of bottle</Typography.Title>
                             <Select 
-                                value={fields.typeOfBottle.value()} 
-                                onChange={(value) => fields.typeOfBottle.set(value)}
-                                options={ALCOHOL_BOTTLE_TYPE_OPTIONS_MAP[fields.name.value()]}
+                                value={drink.typeOfBottle.value()} 
+                                onChange={(value) => drink.typeOfBottle.set(value)}
+                                options={ALCOHOL_BOTTLE_TYPE_OPTIONS_MAP[drink.name.value() as AlcoholType]}
                             />
                         </Flex>
                         <Flex style={{ minWidth: 200 }} vertical gap={4} flex={1}>
                             <Typography.Title level={5}>Size of bottle</Typography.Title>
                             <Select 
-                                value={fields.sizeOfBottle.value()} 
-                                onChange={(value) => fields.sizeOfBottle.set(value)}
-                                options={fields.typeOfBottle.value() ? BOTTLE_SIZE_OPTIONS_MAP[fields.typeOfBottle.value()] : []}
-                                disabled={!fields.typeOfBottle.value()}
+                                value={drink.sizeOfBottle.value()} 
+                                onChange={(value) => drink.sizeOfBottle.set(value)}
+                                options={drink.typeOfBottle.value() ? BOTTLE_SIZE_OPTIONS_MAP[drink.typeOfBottle.value()] : []}
+                                disabled={!drink.typeOfBottle.value()}
                             />
                         </Flex>
                         <Flex style={{ minWidth: 200 }} vertical gap={4} flex={1}>
                             <Typography.Title level={5}>Count of drinks</Typography.Title>
-                            <Input value={fields.count.value()} onChange={(e) => fields.count.set(Number(e.target.value))} />
+                            <Input value={drink.count.value()} onChange={(e) => drink.count.set(Number(e.target.value))} />
                         </Flex>
                     </Flex>
-                {/* <Button onClick={onAddDrink}>Add Drink</Button> */}
+                  ))}
+                <Button onClick={onAddDrink}>Add Drink</Button>
                 <Button type="primary" htmlType="submit">Submit</Button>
             </Flex>
         </form>
